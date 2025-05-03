c120e5c7a436cf769334cb6edb10f01ab2faac80
Kriti Singa <kritikumari841436@gmail.com>
2025-05-03 08:19:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,153 @@
+const express =require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Listing =require("./models/listing.js");
+const path=require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+console.log("connected to db");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+
+app.get("/",(req,res)=>{
+res.send("Hi i am root");
+});
+
+
+const validateListing = (req, res, next) =>{
+  let { error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg= error.details.map((el)=> el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+
+};
+//IndexRoute
+
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
+
+//new route
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+    });
+
+//show route
+
+app.get("/listings/:id",async (req,res)=>{
+let {id} = req.params;
+const listing=  await Listing.findById(id);
+res.render("listings/show.ejs",{listing});
+});
+
+//create route
+
+app.post("/listings", validateListing, 
+  wrapAsync(async(req,res, next)=>{
+  
+    const newListing=new Listing(req.body.listing);
+    // if(!newListing.title){
+    //   throw new ExpressError(400, "Title is missing!");
+    // }
+    // if(!newListing.description){
+    //   throw new ExpressError(400, "Description is missing!");
+    // }
+    // if(!newListing.location){
+    //   throw new ExpressError(400, "Location is missing!");
+    // }
+    await newListing.save();
+    res.redirect("/listings");
+   }));
+//Edit Rote
+
+// app.get("/listings/:id/edit",async(req,res)=>{
+//     let {id} = req.params;
+//     const listing =  await Listing.findById(id);
+//     res.render("listings/edit.ejs",{listing});
+
+// });
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const Id = id.trim(); // Make sure this line is present!
+    const listing = await Listing.findById(Id);
+      res.render("listings/edit.ejs", { listing });
+  });
+
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  if (!req.body.listing){
+    throw new ExpressError(400, "Send valid data for listing");
+  }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  });
+
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  });
+
+  // app.all("*", (err,req, res, next) => {
+  //   next(new ExpressError(404, "Page is not found"));
+  // });
+  
+  // app.use((err, req, res, next) => {
+  //   let {statusCode, message} = err;
+  //   res.status(statusCode).send(message);
+  // });
+
+
+
+  // Catch-all route for 404 errors
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page is not found"));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err; 
+  res.status(statusCode) .render("error.ejs", { message});
+  // res.status(statusCode).send(message);
+});
+
+
+
+app.listen(8080,()=>{
+console.log("server is listening to 8080");
+});
